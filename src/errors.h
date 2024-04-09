353185f541c779e56dd689a4b17e0189a413c195
hyeongi <77052813+hyeongikas@users.noreply.github.com>
2024-04-09 03:18:53 +0200

    Update errors.h

--- src/errors.h
+++ src/errors.h
@@ -1,75 +1,79 @@
-#ifndef _ERRORS_H
-#define _ERRORS_H
+#ifndef ERRORS_H
+#define ERRORS_H
 
 #include <iostream>
 #include <string>
-using namespace std;
-
-class Argument_error {
-    string errmsg;
-public:
-    friend ostream& operator << (ostream& os, const Argument_error& ae) {
-        os << "Argument error: " << ae.errmsg;
-        return os;
-    }
-    
-    explicit Argument_error(const char * str) : errmsg(str) {}
-};
-
-class File_open_error {
-    string filename;
-public:
-    friend ostream& operator << (ostream& os, const File_open_error& fe) {
-        os << "Error opening " << fe.filename;
-        return os;
-    }
-
-    explicit File_open_error(const string& name) : filename(name) {}
-};
 
 class Parse_error {
 public:
-    virtual void print_self(ostream& os) const {
+    virtual void print_self(std::ostream& os) const {
         os << "unspecified.";
     }
 
-    friend ostream& operator << (ostream& os, const Parse_error& pe) {
+    friend std::ostream& operator<<(std::ostream& os, const Parse_error& pe) {
         os << "Parse error: ";
         pe.print_self(os);
         return os;
     }
+
     virtual ~Parse_error() {}
 };
 
-class Parse_error_str : public Parse_error {
-    string str;
+class Argument_error : public Parse_error {
+    std::string errmsg;
+
 public:
-    virtual void print_self(ostream& os) const {
+    Argument_error(const char* str) : errmsg(str) {}
+
+    void print_self(std::ostream& os) const override {
+        os << "Argument error: " << errmsg;
+    }
+};
+
+class File_open_error : public Parse_error {
+    std::string filename;
+
+public:
+    File_open_error(const std::string& name) : filename(name) {}
+
+    void print_self(std::ostream& os) const override {
+        os << "Error opening " << filename;
+    }
+};
+
+class Parse_error_str : public Parse_error {
+    std::string str;
+
+public:
+    explicit Parse_error_str(std::string s) : str(std::move(s)) {}
+
+    void print_self(std::ostream& os) const override {
         os << str;
     }
-
-    explicit Parse_error_str(string s) : str(s) {}
 };
 
 class Size_mismatch : public Parse_error {
-    const unsigned long real_size, read_size;
+    unsigned long real_size;
+    unsigned long read_size;
+
 public:
-    virtual void print_self(ostream& os) const {
+    Size_mismatch(unsigned long real_s, unsigned long read_s)
+        : real_size(real_s), read_size(read_s) {}
+
+    void print_self(std::ostream& os) const override {
         os << "expected " << real_size << " bits, read " << read_size;
     }
-
-    Size_mismatch(unsigned long real_s, unsigned long read_s) : real_size(real_s), read_size(read_s) {}
 };
 
 class Invalid_id : public Parse_error {
-    const int id;
+    int id;
+
 public:
-    virtual void print_self(ostream& os) const {
+    explicit Invalid_id(int i) : id(i) {}
+
+    void print_self(std::ostream& os) const override {
         os << "invalid codebook id " << id << ", try --inline-codebooks";
     }
-
-    explicit Invalid_id(int i) : id(i) {}
 };
 
-
-#endif
+#endif // ERRORS_H
